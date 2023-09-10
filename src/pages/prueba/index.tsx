f903a0fc41642519851f87366857bdfa69d7f6d9
Emmanuel <ema-stuff@hotmail.com>
2023-09-10 11:24:03 -0700

    adding pictures and fonts

---
+++ src/pages/prueba/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+type Props = {}
+const index = (props: Props) => {
+  return (
+    <>
+    <div className={styles.Title}>My own text </div>
+    <div>My own text </div>
+    </>
+  )
+}
+
+export default index
