c489f11a214178a204d7064c69148880895c49dc
Liliana Gallegos <lily.gallegos@hotmail.com>
2023-11-04 02:11:22 -0600

    trying to create new authors page

---
+++ src/pages/authors/index.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+import styles from '../../app/page.module.css'
+
+
+export default function Authors() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <Image src='/logo.png' alt='Stardew Valley Codes' width={255} height={143} priority/>
+      </div>
+      <div className={styles.frame}>
+        <div className={styles.base}>
+          <div className={styles.square} style={{ width: '166px', height: '154px'}}>
+            <Image src='/portraitLilita.png' alt='Stardew Valley Codes' width={170} height={170}/>
+          </div>
+          <div className={styles.square} style={{ width: '166px', height: '154px'}}>
+            <Image src='/portraitEmmita.png' alt='Stardew Valley Codes' width={170} height={170}/>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
