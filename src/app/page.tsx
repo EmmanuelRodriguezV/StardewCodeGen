55bd1819501f8701734e6e0966b8b26a3b3ac533
Liliana Gallegos <lily.gallegos@hotmail.com>
2023-11-04 01:45:26 -0600

    saving images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <div>
+        <Image src='/logo.png' alt='Stardew Valley Codes' width={255} height={143}/>
+      </div>
       <div className={styles.frame}>
         <div className={styles.base}>
           <div className={styles.square}></div>
