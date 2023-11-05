500cdb25392363bae318d4c229b1a56116becf0f
Liliana Gallegos <lily.gallegos@hotmail.com>
2023-11-04 18:44:54 -0600

    create square item

--- src/app/components/squareItem/index.tsx
+++ src/app/components/squareItem/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import styles from '../../page.module.css'
+import Image from 'next/image'
 
-export default function SquareItem() {
+export default function SquareItem({data}: any) {
   return (
-    <div>SquareItem</div>
+    <div className={styles.square}>
+      <Image alt={data.name} width={60} height={60} src={data.route}/>
+    </div>
   )
 }
