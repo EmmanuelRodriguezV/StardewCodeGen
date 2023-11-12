f599996717b0d48d2db2147527859be17356af5a
Liliana Gallegos <lily.gallegos@hotmail.com>
2023-11-12 01:11:09 -0600

    add tooltip styles

--- src/app/components/tooltip/index.tsx
+++ src/app/components/tooltip/index.tsx
@@ -6,9 +6,8 @@ import styles from '../../styles/Layout.module.scss'
 export default function Tooltip({item, top, left}: any) {
   return (
     <div className={`${styles.frame} ${styles.toolTip}`} style={{top:top, left: left}}>
-      <div className={`${styles.base} ${styles.toolTip}`}>
-        {item.itemTitle}
-        <br></br>
+      <div className={`${styles.base} ${styles.toolTip}`} >
+        <p style={{fontWeight:'bold', marginBottom: '5px'}}>{item.itemTitle}</p>
         <p>{item.description}</p>
       </div>
     </div>
