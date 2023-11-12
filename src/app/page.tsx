f9b7a40e9b995a33aef3d1f61e4d33043289b67a
Emmanuel <ema-stuff@hotmail.com>
2023-11-12 12:13:59 -0800

    adding search functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,39 @@ import styles from './styles/Layout.module.scss'
 import SquareItem from './components/squareItem'
 import data from '../../generateCodes/StardewItemCodes.json';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useState,useEffect, KeyboardEvent,ChangeEvent } from 'react';
 export default function Home() {
+  type IdataStar = {
+    code:string,
+    itemTitle:string,
+    description:string,
+    srcImageName?:string,
+  }
   const [line, setLine] = useState(true)
+  const [dataCopy,setData] = useState<Array<IdataStar>>([]); 
+  const searchItem = (e: ChangeEvent<HTMLInputElement>): void => {
+    const searchTerm = e.currentTarget.value.toLowerCase();
+    const regex = new RegExp(searchTerm, 'i'); // 'i' flag for case-insensitive matching
+
+    setData(data.filter(d => regex.test(d.itemTitle) && d.srcImageName));
+  }; 
+  const resetItems = (e:KeyboardEvent<HTMLInputElement>):void =>{
+    if(e.currentTarget.value ==='')
+    setData(data);
+  }
+  useEffect(()=>{
+    setData(data )
+  },[])
   return (
     <main className={styles.main}>
       <div className={styles.top}>
-        <div className={styles.toogleBtn} onClick={()=> setLine(!line)}>
-          <div className={styles.baseBtn}>
-            { line ? <Image alt='list' src={'/icons/line3.svg'} width={20} height={20} /> :
-            <Image alt='square' src={'/icons/grid.svg'} width={30} height={30} />
-            }
-          </div>
-        </div>
+        <input onKeyUp={resetItems} onKeyDown={searchItem} type='text' placeholder='search'></input>
+        {(buttonToggle())}
       </div>
       <div className={`${styles.frame} ${styles.fullScreen}`}>
         <div className={`${styles.base} ${styles.fullScreen}`}>
           {
-            data.map((item, index) => {
+            dataCopy.map((item, index) => {
              return <SquareItem key={index} data={item} />
             })
           }
@@ -28,4 +43,13 @@ export default function Home() {
       </div>
     </main>
   )
+
+  function buttonToggle() {
+    return <div className={styles.toogleBtn} onClick={() => setLine(!line)}>
+      <div className={styles.baseBtn}>
+        {line ? <Image alt='list' src={'/icons/line3.svg'} width={20} height={20} /> :
+          <Image alt='square' src={'/icons/grid.svg'} width={30} height={30} />}
+      </div>
+    </div>;
+  }
 }
